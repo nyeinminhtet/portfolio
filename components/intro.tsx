026f0183031aae32c66c60810636330442fe725e
Two J <TwoJ@gmail.com>
2023-08-28 12:42:25 +0630

    chore

--- components/intro.tsx
+++ components/intro.tsx
@@ -82,8 +82,7 @@ const Intro = () => {
           Borderblack outline-none focus:scale-110 hover:scale-110 active:scale-105 transition dark:bg-white/10 dark:text-gray-200"
           href="/Cv.pdf"
         >
-          Download CV{" "}
-          <HiDownload className=" opacity-60 group-hover:translate-y-1 transition" />{" "}
+          Resume{" "}
         </a>
 
         <div className="flex gap-2">
